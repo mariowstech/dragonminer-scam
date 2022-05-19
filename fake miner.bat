cc47effd8e23b1c91ed3f92469f5bed6e4b17d4f
NotNotepad <102176023+MariowIsUseless@users.noreply.github.com>
2022-05-19 22:25:46 +0200

    Add files via upload

---
+++ fake miner.bat
@@ -0,0 +1,42 @@
+echo off
+title Dragon Miner 
+cls
+:main
+color 9                                                                                                       
+echo DDDDDDDDDDDDD                                                                                                  
+echo D::::::::::::DDD                                                                                               
+echo D:::::::::::::::DD                                                                                             
+echo DDD:::::DDDDD:::::D                                                                                            
+echo D:::::D    D:::::D rrrrr   rrrrrrrrr   aaaaaaaaaaaaa     ggggggggg   ggggg   ooooooooooo   nnnn  nnnnnnnn    
+echo D:::::D     D:::::Dr::::rrr:::::::::r  a::::::::::::a   g:::::::::ggg::::g oo:::::::::::oo n:::nn::::::::nn  
+echo D:::::D     D:::::Dr:::::::::::::::::r aaaaaaaaa:::::a g:::::::::::::::::go:::::::::::::::on::::::::::::::nn 
+echo D:::::D     D:::::Drr::::::rrrrr::::::r         a::::ag::::::ggggg::::::ggo:::::ooooo:::::onn:::::::::::::::n
+echo D:::::D     D:::::D r:::::r     r:::::r  aaaaaaa:::::ag:::::g     g:::::g o::::o     o::::o  n:::::nnnn:::::n
+echo D:::::D     D:::::D r:::::r     rrrrrrraa::::::::::::ag:::::g     g:::::g o::::o     o::::o  n::::n    n::::n
+echo D:::::D     D:::::D r:::::r           a::::aaaa::::::ag:::::g     g:::::g o::::o     o::::o  n::::n    n::::n
+echo D::::::D    D:::::D  r:::::r          a::::a    a:::::ag::::::g    g:::::g o::::o     o::::o  n::::n    n::::n
+echo DDD:::::DDDDD:::::D r:::::r          a::::a    a:::::ag:::::::ggggg:::::g o:::::ooooo:::::o  n::::n    n::::n
+echo D:::::::::::::::DD   r:::::r          a:::::aaaa::::::a g::::::::::::::::g o:::::::::::::::o  n::::n    n::::n
+echo D::::::::::::DDD      r:::::r           a::::::::::aa:::a gg::::::::::::::g  oo:::::::::::oo   n::::n    n::::n
+echo DDDDDDDDDDDDD         rrrrrrr            aaaaaaaaaa  aaaa   gggggggg::::::g    ooooooooooo     nnnnnn    nnnnnn
+echo                                                                     g:::::g                                    
+echo                                                         gggggg      g:::::g                                    
+echo                                                         g:::::gg   gg:::::g                                    
+echo                                                          g::::::ggg:::::::g                                    
+echo                                                           gg:::::::::::::g                                     
+echo                                                             ggg::::::ggg                                       
+echo                                                                gggggg                                          
+echo ==============================================================================================================================================
+echo Wallet: 3847723874wherc87g623874c6281usd
+echo Balance: 1287723712778₿
+echo Key: 73489723648762348762v8736487v6238746v86h4s383728327498273hxc4x87g167
+set/p input=Start mining? [Y/N]
+if %input%==Y goto Miner
+if %input%==N goto main
+:Miner
+cls
+color 4
+cls
+:mining
+echo ATTEMPTING TO MINE FROM: 783847297Q7342830942830C4J7273487DH6........... [UNSUCCESFUL]
+goto mining

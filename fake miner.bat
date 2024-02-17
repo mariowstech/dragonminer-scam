ed7bd0a8faca03760e346fa02b923409ddd0d8d0
mariow's tech! <102176023+mariowstech@users.noreply.github.com>
2024-02-17 21:22:04 +0100

    v3 - FINALLY FIXED THE RANDOM NUMBER!!

--- fake miner.bat
+++ fake miner.bat
@@ -11,14 +11,14 @@ echo DD   DD rr     aa  aaa ggggggg oo  oo nn   nn    mmm  mm  mm iii nn   nn ee
 echo DDDDDD  rr      aaa aa      gg  oooo  nn   nn    mmm  mm  mm iii nn   nn  eeeee rr     
 echo                         ggggg                                                                                         
 echo =========================================================================================
-echo Wallet: 3847723874wherc87g623874c6281usd
-echo Balance: 0₿
+echo Wallet: 3847723874sh87i623874t6281ty
+echo Balance: 0
 echo Key: ********************UWS
-------------------------------------
+echo ------------------------------------
 echo Y = start mining
 echo A = about
 echo N = close
-set/p input=]
+set/p input=:
 if %input%==Y goto Miner
 if %input%==N exit
 if %input%==a goto about
@@ -27,10 +27,10 @@ cls
 color 4
 cls
 :mining
-set /a whole=(%RANDOM%*max/327686)+min
-set /a decimal=(%RANDOM%*max/32768)+min
-echo ATTEMPTING TO MINE FROM: %whole%.%decimal% ........... [UNSUCCESFUL]
-echo ATTEMPTING TO MINE FROM: %whole%.%decimal% ........... [UNSUCCESFUL]
+set /a min = 34578964
+set /a max = 97894357
+set /a rndNum=( %RANDOM% %% (%max% - %min% + 1) ) + %min%
+echo ATTEMPTING TO MINE FROM: %rndNum% .....[UNSUCCESFUL]
 goto mining
 
 :about
@@ -42,19 +42,19 @@ echo mmm  mm  mm aa  aaa rr     iii oo  oo  ww ww ww   sss  tt    eeeee  cc     
 echo mmm  mm  mm  aaa aa rr     iii  oooo    ww  ww       s  tttt  eeeee  ccccc hh   hh !!! 
 echo                                                   sss                                  
 echo ---------------------------------------------
-echo Dragon miner v2 (7/6/2023) - IFLK 1.2.1883
+echo Dragon miner v3 (17/2/2024) - IFLK 2.0.0
 echo ---------------------------------------------
 echo This is a joke program to make fun of kids that sell fake bitcoin miners.
 echo Please don't take it seriously, this is a harmless program, made in batch.
 echo Take this as an example, and how kids make money from these programs.
 echo Please don't reuse this sample as a way to make money. mariowstech's stuff will always
-echo remain free and open-source!
+echo remain free and open-source! Ill somehow keep this also updated sometimes.
 echo ---------------------------------------------
 echo Repo: https://github.com/mariowstech/dragonminer-scam
 echo Made with hate by mariowstech!
 echo -
 echo More stuff: https://github.com/mariowstech/ - https://tiktok.com/@mariowstech
-echo -           https://youtube.com/@mariowstech 
+echo -                           https://youtube.com/@mariowstech 
 echo (Press any key to return to main menu)
 pause >nul
 goto main
